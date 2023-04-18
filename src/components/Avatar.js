4961a338a02c0308537d5563f65b4c617e25b7f7
Tyler DeBoer <tylercdeboer@gmail.com>
2023-04-18 14:34:24 -0500

    Avatar props and styles

---
+++ src/components/Avatar.js
@@ -0,0 +1,230 @@
+import styled, { css } from "styled-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ScreenReaderContent from './shared/ScreenReaderContent';
+import { hintTooltipStyles } from "./shared/styles";
+import { hintTooltipStyless } from "./shared/styles";
+// import AvatarInitials from './shared/components/AvatarInitials';
+// import { avatarSizes } from './shared/styles';
+
+const tooltipPositioning = {
+  'top': {
+    top: "auto",
+    bottom: "100%",
+    right: "auto",
+    left: "auto",
+    transform: "translateY(-11px)",
+    borderColor: "var(--cc_color_button_background_secondary) transparent transparent transparent",
+  },
+  'top-right': {
+    top: "auto",
+    bottom: "100%",
+    right: "auto",
+    left: "0",
+    transform: "translateY(-11px)",
+    borderColor: "var(--cc_color_button_background_secondary) transparent transparent transparent",
+  },
+  'top-left': {
+    top: "auto",
+    bottom: "100%",
+    right: "0",
+    left: "auto",
+    transform: "translateY(-11px)",
+    borderColor: "var(--cc_color_button_background_secondary) transparent transparent transparent",
+  },
+  'bottom': {
+    top: "100%",
+    bottom: "auto",
+    right: "auto",
+    left: "auto",
+    transform: "translateY(11px)",
+    borderColor: "transparent transparent var(--cc_color_button_background_secondary) transparent",
+  },
+  'bottom-right': {
+    top: "100%",
+    bottom: "auto",
+    right: "auto",
+    left: "0",
+    transform: "translateY(11px)",
+    borderColor: "transparent transparent var(--cc_color_button_background_secondary) transparent",
+  },
+  'bottom-left': {
+    top: "100%",
+    bottom: "auto",
+    right: "0",
+    left: "auto",
+    transform: "translateY(11px)",
+    borderColor: "transparent transparent var(--cc_color_button_background_secondary) transparent",
+  },
+};
+
+const hintTooltipStylesss = css`
+  position: relative;
+  overflow: visible;
+
+  &::before,
+  &::after {
+    position: absolute;
+    visibility: hidden;
+    opacity: 0;
+    z-index: 1000000;
+    pointer-events: none;
+
+    ${(props) => css`
+      top: ${tooltipPositioning[props.tooltipPosition].top};
+      bottom: ${tooltipPositioning[props.tooltipPosition].bottom};
+    `}
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    background: transparent;
+    border: 6px solid;
+    z-index: 1000001;
+
+    ${(props) => css`
+      border-color: ${tooltipPositioning[props.tooltipPosition].borderColor};
+    `}
+  }
+
+  &::after {
+    content: attr(aria-label);
+    padding: 6px 8px;
+
+    background: var(--cc_color_button_background_secondary);
+    border-radius: var(--cc_size_border_radius_l);
+    box-shadow: none;
+
+    color: var(--cc_color_button_text_secondary);
+    font-weight: 500;
+    font-size: 11px;
+    line-height: 1.5;
+    white-space: nowrap;
+    text-shadow: none;
+
+    ${(props) => css`
+      transform: ${tooltipPositioning[props.tooltipPosition].transform};
+      left: ${tooltipPositioning[props.tooltipPosition].left};
+      right: ${tooltipPositioning[props.tooltipPosition].right};
+    `}
+  }
+
+  &:hover::before,
+  &:hover::after {
+    visibility: visible;
+    opacity: 1;
+  }
+`;
+
+const avatarSizes = {
+  large: 40,
+  medium: 32,
+  small: 24,
+  tiny: 16,
+};
+
+const Image = styled.div`
+  position: relative;
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100%;
+  overflow: hidden;
+  height: ${(props) => `${props.avatarSize}px`};
+  width: ${(props) => `${props.avatarSize}px`};
+  line-height: ${(props) => `${props.avatarSize}px`};
+  background-image: url(${(props) => props.imageSrc});
+  background-size: cover;
+  background-position: 50%;
+  background-color: ${(props) => props.backgroundColor ? props.backgroundColor : 'var(--cc_color_background_3)'};
+  border: 2px solid;
+  border-color: ${(props) => props.color ? props.color : 'transparent'};
+  color: ${(props) => props.textColor ? props.textColor : 'var(--cc_color_text_subtle)'};
+  ${(props) => props.showTitleOnHover ? hintTooltipStylesss : null};
+  /* ${(props) => props.showTitleOnHover ? hintTooltipStyless({ position: props.tooltipPosition }) : null} */
+`;
+
+const Initials = styled.div`
+  font-size: ${(props) => `${props.textSize}px`};
+  font-weight: 600;
+  text-align: center;
+  /* color: var(--cc_color_text_subtle); */
+  font-family: 'Averta', system-ui, sans-serif;
+`;
+
+const AvatarInitials = ({ initials, avatarSize }) => {
+  const textSize = Math.round(avatarSize / 2.3125);
+  return <Initials textSize={textSize}>{initials}</Initials>;
+};
+
+const Avatar = ({
+  title,
+  src,
+  color,
+  backgroundColor,
+  textColor,
+  size,
+  className,
+  style,
+  showTitleOnHover,
+  tooltipPosition,
+}) => {
+  const avatarSize = typeof size === 'number' ? size : avatarSizes[size];
+  const initials = title ? title.split(' ').map(x => x.charAt(0)).join('').substr(0, 3).toUpperCase() : null
+  return (
+    <Image
+      className={className}
+      avatarSize={avatarSize}
+      color={color}
+      backgroundColor={backgroundColor}
+      textColor={textColor}
+      imageSrc={src}
+      aria-label={title}
+      style={style}
+      showTitleOnHover={initials && showTitleOnHover}
+      tooltipPosition={tooltipPosition}
+    >
+      {!src && !initials ? (
+        <i className="mdi mdi-account" />
+      ) : null}
+      {!src && initials ? (
+        <AvatarInitials textColor={textColor} avatarSize={avatarSize} initials={initials} />
+      ) : null}
+      {title ? <ScreenReaderContent>{title}</ScreenReaderContent> : null}
+    </Image>
+  );
+};
+
+Avatar.propTypes = {
+  title: PropTypes.string,
+  src: PropTypes.string,
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  size: PropTypes.oneOfType([
+    PropTypes.oneOf(["tiny", "small", "medium", "large"]),
+    PropTypes.number,
+  ]),
+  className: PropTypes.string,
+  style: PropTypes.object,
+  showTitleOnHover: PropTypes.bool,
+  tooltipPosition: PropTypes.oneOf(["top", "top-left", "top-right", "bottom", "bottom-left", "bottom-right"]),
+};
+
+Avatar.defaultProps = {
+  title: undefined,
+  src: '',
+  size: avatarSizes.medium,
+  color: undefined,
+  backgroundColor: undefined,
+  textColor: undefined,
+  className: undefined,
+  style: undefined,
+  showTitleOnHover: false,
+  tooltipPosition: "top",
+};
+
+export default Avatar;
--- src/components/shared/styles.js
+++ src/components/shared/styles.js
@@ -132,3 +132,277 @@ export const spacingTokensToCSS = (spacing) => {
   });
   return spacingToTokens.join(" ");
 };
+
+/*
+ * --- HINT TOOLTIP
+ */
+
+export const hintTooltipStyles = ({ position }) => css`
+  position: relative;
+  overflow: visible;
+
+  &::before,
+  &::after {
+    position: absolute;
+    bottom: 100%;
+    bottom: ${(props) => props.position === 'top' ? '100%' : 0};
+    left: 50%;
+    transform: translate3d(0, 0, 0);
+    visibility: hidden;
+    opacity: 0;
+    z-index: 1000000;
+    pointer-events: none;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    margin-bottom: -7px;
+    /* transform: translateX(-50%) translateY(-5px); */
+    background: transparent;
+    border: 6px solid transparent;
+    border-top-color: var(--cc_color_button_background_secondary);
+    z-index: 1000001;
+  }
+
+  &::after {
+    /* left: 8px; */
+    padding: 6px 8px;
+
+    /* transform: translateX(-8px) translateY(-9px); */
+    /* transform: translateX(-50%) translateY(-9px); */
+
+    background: var(--cc_color_button_background_secondary);
+    border-radius: var(--cc_size_border_radius_l);
+    box-shadow: none;
+
+    color: var(--cc_color_button_text_secondary);
+    font-weight: 500;
+    /* font-family: $systemFontsStack; */
+    font-size: 11px;
+    line-height: 1.5;
+    white-space: nowrap;
+    text-shadow: none;
+  }
+
+  &:hover::before,
+  &:hover::after {
+    visibility: visible;
+    opacity: 1;
+  }
+
+  &:hover::before {
+    transform: translateX(-50%) translateY(-6px);
+  }
+
+  &:hover::after {
+    transform: translateX(-50%) translateY(-10px);
+  }
+
+  &[aria-label]::after {
+    content: attr(aria-label);
+  }
+`;
+
+const tooltipBefore = (position) => {
+  switch (position) {
+    case 'top':
+      return css`
+        position: absolute;
+        transform: translate3d(0, 0, 0);
+        visibility: hidden;
+        opacity: 0;
+        z-index: 1000001;
+        pointer-events: none;
+        left: 50%;
+        bottom: 100%;
+        margin-bottom: -7px;
+        border: 6px solid transparent;
+        border-top-color: var(--cc_color_button_background_secondary);
+      `;
+    case 'top-left':
+      return css`
+        left: 50%;
+        bottom: 100%;
+        margin-bottom: -7px;
+        border: 6px solid transparent;
+        border-top-color: var(--cc_color_button_background_secondary);
+      `;
+    case 'top-right':
+      return css`
+        left: 50%;
+        bottom: 100%;
+        margin-bottom: -7px;
+        border: 6px solid transparent;
+        border-top-color: var(--cc_color_button_background_secondary);
+      `;
+    case 'bottom':
+      return css`
+        transform-origin: right center;
+      `;
+    case 'bottom-left':
+      return css`
+        transform-origin: top left;
+      `;
+    case 'bottom-right':
+      return css`
+        transform-origin: top right;
+      `;
+  }
+};
+
+const tooltipHoverBefore = (position) => {
+  switch (position) {
+    case 'top':
+      return css`
+        transform: translateX(-50%) translateY(-6px);
+      `;
+    case 'top-left':
+      return css`
+        transform: translateX(-50%) translateY(-6px);
+      `;
+    case 'top-right':
+      return css`
+        transform: translateX(-50%) translateY(-6px);
+      `;
+    case 'bottom':
+      return css`
+        transform-origin: right center;
+      `;
+    case 'bottom-left':
+      return css`
+        transform-origin: top left;
+      `;
+    case 'bottom-right':
+      return css`
+        transform-origin: top right;
+      `;
+  }
+};
+
+const tooltipAfter = (position) => {
+  switch (position) {
+    case 'top':
+      return css`
+        position: absolute;
+        transform: translate3d(0, 0, 0);
+        visibility: hidden;
+        opacity: 0;
+        z-index: 1000000;
+        pointer-events: none;
+        left: 50%;
+        bottom: 100%;
+      `;
+    case 'top-left':
+      return css`
+        right: 0;
+        bottom: 100%;
+      `;
+    case 'top-right':
+      return css`
+        left: 0;
+        bottom: 100%;
+      `;
+    case 'bottom':
+      return css`
+        transform-origin: right center;
+      `;
+    case 'bottom-left':
+      return css`
+        transform-origin: top left;
+      `;
+    case 'bottom-right':
+      return css`
+        transform-origin: top right;
+      `;
+  }
+};
+
+const tooltipHoverAfter = (position) => {
+  switch (position) {
+    case 'top':
+      return css`
+        transform: translateX(-50%) translateY(-10px);
+      `;
+    case 'top-left':
+      return css`
+        transform: translateX(50%) translateY(-10px);
+      `;
+    case 'top-right':
+      return css`
+        transform: translateX(-50%) translateY(-10px);
+      `;
+    case 'bottom':
+      return css`
+        transform-origin: right center;
+      `;
+    case 'bottom-left':
+      return css`
+        transform-origin: top left;
+      `;
+    case 'bottom-right':
+      return css`
+        transform-origin: top right;
+      `;
+  }
+};
+
+
+export const hintTooltipStyless = ({ position }) => {
+  return`
+    position: relative;
+    overflow: visible;
+
+    &::before,
+    &::after {
+      position: absolute;
+      transform: translate3d(0, 0, 0);
+      visibility: hidden;
+      opacity: 0;
+      z-index: 1000000;
+      pointer-events: none;
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      background: transparent;
+      z-index: 1000001;
+      ${tooltipBefore(position)}
+    }
+
+    &::after {
+      padding: 6px 8px;
+
+      background: var(--cc_color_button_background_secondary);
+      border-radius: var(--cc_size_border_radius_l);
+      box-shadow: none;
+
+      color: var(--cc_color_button_text_secondary);
+      font-weight: 500;
+      font-size: 11px;
+      line-height: 1.5;
+      white-space: nowrap;
+      text-shadow: none;
+      ${tooltipAfter(position)};
+    }
+
+    &:hover::before,
+    &:hover::after {
+      visibility: visible;
+      opacity: 1;
+    }
+
+    &:hover::before {
+      ${tooltipHoverBefore(position)};
+    }
+
+    &:hover::after {
+      ${tooltipHoverBefore(position)};
+    }
+
+    &[aria-label]::after {
+      content: attr(aria-label);
+    }
+  `;
+};
